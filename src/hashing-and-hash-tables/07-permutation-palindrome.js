aae220e4ea1da81d22687618a005e7a8ab772536
Amber Johnson <amberjohnsonsmile@gmail.com>
2023-11-30 20:05:25 -0700

    Refactor palindrome permutation function

--- src/hashing-and-hash-tables/07-permutation-palindrome.js
+++ src/hashing-and-hash-tables/07-permutation-palindrome.js
@@ -1,22 +1,11 @@
 export function isPalindromeOrPermutation(characters) {
-  // Count the characters
-  const characterHash = {}
-  for (let i = 0; i < characters.length; i++) {
-    const current = characters[i]
-    if (current in characterHash) {
-      characterHash[current] += 1
+  const unpairedSet = new Set()
+  for (let char of characters) {
+    if (unpairedSet.has(char)) {
+      unpairedSet.delete(char)
     } else {
-      characterHash[current] = 1
+      unpairedSet.add(char)
     }
   }
-
-  // There can only be one character that has an odd count in a palindrome
-  let oddEntries = 0
-  Object.values(characterHash).forEach((count) => {
-    if (count % 2 != 0) {
-      oddEntries++
-    }
-  })
-
-  return oddEntries <= 1
+  return unpairedSet.size <= 1
 }
