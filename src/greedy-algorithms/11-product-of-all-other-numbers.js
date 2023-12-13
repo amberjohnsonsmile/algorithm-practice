3ac7ecdf223afec0c3712228b237bcd116bbb7b7
Amber Johnson <amberjohnsonsmile@gmail.com>
2023-12-13 13:06:47 -0700

    Refactor to use only one array

--- src/greedy-algorithms/11-product-of-all-other-numbers.js
+++ src/greedy-algorithms/11-product-of-all-other-numbers.js
@@ -1,22 +1,16 @@
 export function getProductOfAllIntsExceptAtIndex(intArray) {
   const productArray = []
-  let productsBeforeIndex = []
-  let productsAfterIndex = []
 
   let productSoFar = 1
   for (let i = 0; i < intArray.length; i++) {
-    productsAfterIndex[i] = productSoFar
+    productArray[i] = productSoFar
     productSoFar *= intArray[i]
   }
 
   productSoFar = 1
   for (let i = intArray.length - 1; i >= 0; i--) {
-    productsBeforeIndex[i] = productSoFar
+    productArray[i] = productArray[i] * productSoFar
     productSoFar *= intArray[i]
-  }
-
-  for (let i = 0; i < intArray.length; i++) {
-    productArray[i] = productsBeforeIndex[i] * productsAfterIndex[i]
   }
 
   return productArray
