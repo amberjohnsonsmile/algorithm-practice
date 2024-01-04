e651c6db2f41981a462282b7aeb58066cd490696
Amber Johnson <amberjohnsonsmile@gmail.com>
2024-01-04 12:16:55 -0700

    Finish refactoring test to use class method

--- test/trees-and-graphs/16-balanced-binary-tree.test.js
+++ test/trees-and-graphs/16-balanced-binary-tree.test.js
@@ -6,47 +6,36 @@ describe('treeIsSuperBalanced', function () {
   it('returns true when the diff between any two node depths is 1 or less', function () {
     // Only two leaves and they are at the same depth
     const baseNode = new BinaryTreeNode(getRandomInt())
-    baseNode.left = new BinaryTreeNode(getRandomInt())
-    baseNode.right = new BinaryTreeNode(getRandomInt())
+    baseNode.insertLeft(getRandomInt())
+    baseNode.insertRight(getRandomInt())
 
     const result = treeIsSuperBalanced(baseNode)
     assert.equal(result, true)
   })
 
   it('returns false when the diff between any two node depths is greater than 1', function () {
-    const node1 = new BinaryTreeNode(getRandomInt())
-    const node2 = new BinaryTreeNode(getRandomInt())
-    const node3 = new BinaryTreeNode(getRandomInt())
     const baseNode = new BinaryTreeNode(getRandomInt())
 
-    // Leaf at depth 3
-    node3.left = new BinaryTreeNode(getRandomInt())
-    node2.left = node3
-    node1.left = node2
-    baseNode.left = node1
-    // Leaf at depth 1
-    baseNode.right = new BinaryTreeNode(getRandomInt())
+    baseNode.insertRight(getRandomInt())
+    baseNode.right.insertRight(getRandomInt())
+    baseNode.right.right.insertRight(getRandomInt())
+    baseNode.insertLeft(getRandomInt())
 
     const result = treeIsSuperBalanced(baseNode)
     assert.equal(result, false)
   })
 
   it('returns false when there are more than two depths', function () {
-    // Leaf at depth 3
-    const node1 = new BinaryTreeNode(getRandomInt())
-    const node2 = new BinaryTreeNode(getRandomInt())
-    const node3 = new BinaryTreeNode(getRandomInt())
-    const node4 = new BinaryTreeNode(getRandomInt())
     const baseNode = new BinaryTreeNode(getRandomInt())
 
-    node2.left = node4
-    node2.right = node3
-    node1.left = node2
-    // Leaf at depth 2
-    node1.right = new BinaryTreeNode(getRandomInt())
-    baseNode.left = node1
-    // Leaf at depth 1
-    baseNode.right = new BinaryTreeNode(getRandomInt())
+    // Depth 1
+    baseNode.insertRight(getRandomInt())
+    baseNode.insertLeft(getRandomInt())
+    // Depth 2
+    baseNode.left.insertRight(getRandomInt())
+    baseNode.left.insertLeft(getRandomInt())
+    // Depth 3
+    baseNode.left.left.insertLeft(getRandomInt())
 
     const result = treeIsSuperBalanced(baseNode)
     assert.equal(result, false)
