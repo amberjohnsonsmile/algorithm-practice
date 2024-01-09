6922aabcf3080cb88ba94ab068e5f456eb819b8d
Amber Johnson <amberjohnsonsmile@gmail.com>
2024-01-09 11:36:13 -0700

    Work on graph coloring algorithm

--- src/trees-and-graphs/19-graph-coloring.js
+++ src/trees-and-graphs/19-graph-coloring.js
@@ -1,12 +1,57 @@
-// Color a graph using maxDegree + 1 colors
-export function colorGraph(graphNode, maxDegree) {
-  // Colors will be represented by an array of numbers
-  let currentNode = graphNode
-  let colors = getColors(maxDegree + 1)
+export function colorGraph(graph, colors) {
+  graph.forEach((node) => {
+    if (node.neighbors.has(node)) {
+      throw new Error(
+        `Legal coloring impossible for node with loop: ${node.label}`
+      )
+    }
 
-  while (currentNode) {
-    // Set the color as long as the adjacent neighbors dont' have that color
-  }
+    // Get the node's neighbors' colors, as a set so we
+    // can check if a color is illegal in constant time
+    const illegalColors = new Set()
 
-  return graphNode
+    node.neighbors.forEach((neighbor) => {
+      if (neighbor.color !== null) {
+        illegalColors.add(neighbor.color)
+      }
+    })
+
+    // Assign the first legal color
+    for (let i = 0; i < colors.length; i++) {
+      const color = colors[i]
+
+      if (!illegalColors.has(color)) {
+        node.color = color
+        break
+      }
+    }
+  })
+  return graph
 }
+
+// // Color a graph using maxDegree + 1 colors
+// export function colorGraph(graphNode, maxDegree) {
+//   const nodes = [graphNode]
+//   let currentColor = 0
+
+//   while (nodes.length) {
+//     const currentNode = nodes.shift()
+//     currentNode.color = currentColor
+//     currentColor = getNextColor(currentColor, maxDegree)
+
+//     currentNode.neighbors.forEach((neighbor) => {
+//       if (!neighbor.color) {
+//         nodes.push(neighbor)
+//       }
+//     })
+//   }
+//   return graphNode
+// }
+
+// function getNextColor(color, maxDegree) {
+//   if (color + 1 > maxDegree + 1) {
+//     return 0
+//   } else {
+//     return color + 1
+//   }
+// }
--- test/trees-and-graphs/19-graph-coloring.test.js
+++ test/trees-and-graphs/19-graph-coloring.test.js
@@ -1,14 +1,23 @@
 import * as assert from 'assert'
-import { colorGraph } from '../../src/trees-and-graphs/19-graph-coloring'
-import { GraphNode } from './graph-node'
+import { colorGraph } from '../../src/trees-and-graphs/19-graph-coloring.js'
+import { GraphNode } from './graph-node.js'
 
 describe('colorGraph', function () {
   it('returns a legally colored graph using maxDegree + 1 colors', function () {
-    const graphNode = new GraphNode('Harry')
-    graphNode.neighbors.add(new GraphNode('Bella'))
-    graphNode.neighbors.add(new GraphNode('Katniss'))
+    const a = new GraphNode('a')
+    const b = new GraphNode('b')
+    const c = new GraphNode('c')
 
-    const result = colorGraph(graphNode, 2)
-    assert.equal(result.color, 1)
+    a.neighbors.add(b)
+    b.neighbors.add(a)
+    c.neighbors.add(b)
+    b.neighbors.add(c)
+
+    const graph = [a, b, c]
+    const result = colorGraph(graph, ['red', 'green', 'blue'])
+    assert.notEqual(result[0].color, result[1].color)
+    assert.notEqual(result[0].color, result[2].color)
+    assert.notEqual(result[1].color, result[2].color)
+    assert.notEqual(result[3].color, result[3].color)
   })
 })
